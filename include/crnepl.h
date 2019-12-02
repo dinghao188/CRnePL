421602737869448c8a5bbafd1d424a7175f6a460
dinghao188 <467682352@qq.com>
2019-12-02 08:52:14 +0800

    a

--- include/crnepl.h
+++ include/crnepl.h
@@ -3,52 +3,109 @@
 
 #include <istream>
 #include <ostream>
+#include <map>
 #include <string>
 #include <vector>
 
-/*
- * class Crnepl
- * The most important to implement RPL function
- */
-class Crnepl
+namespace crnepl
 {
-public:
-    Crnepl();
-    
-    /* @description Add a record to the history queue
-     * @return void
-     */
-    void AddHistory(std::string &record);
     /*
-     * @description Get previous input history
-     * @return the reference of previous record
+     * define all code of key here.
+     * I get it by using bash on linux,
+     * maybe it's different in different operating systems
      */
-    std::string & GetPrevInput();
+    enum KEY_CODE
+    {
+        // Control key
+        KEY_CTRL_A = 1,
+        KEY_CTRL_B, KEY_CTRL_C, KEY_CTRL_D, KEY_CTRL_E, KEY_CTRL_F,
+        KEY_CTRL_G, KEY_CTRL_H, KEY_CTRL_I, KEY_CTRL_J, KEY_CTRL_K,
+        KEY_CTRL_L, KEY_CTRL_M, KEY_CTRL_N, KEY_CTRL_O, KEY_CTRL_P,
+        KEY_CTRL_Q, KEY_CTRL_R, KEY_CTRL_S, KEY_CTRL_T, KEY_CTRL_U,
+        KEY_CTRL_V, KEY_CTRL_W, KEY_CTRL_X, KEY_CTRL_Y, KEY_CTRL_Z,
+
+        // Extend control key start with 27
+        KEY_CTRL_EXT = 27,
+
+        // displayable character code
+        DISPLAY_ASCII_START = 32,   // ' '
+        DISPLAY_ASCII_END = 126,   // '~'
+    };
+
     /*
-     * Get current input history
-     * @return the reference of current record
+     * define all supported actions here
      */
-    std::string & GetCurInput();
+    enum ACTION_CODE
+    {
+        ACT_BS = 1,     // Remove one character forward
+        ACT_DEL,        // Remove one character under current cursor
+        ACT_PRE,        // Get previous command
+        ACT_NEXT,       // Get next command
+        ACT_CLEAR_LINE, // Clear current line input
+        ACT_COMPLETION, // complete current input
+        ACT_SUBMIT      // submit current  input
+    };
+
+    /* simplify the definition of key-action map*/
+    typedef std::pair<KEY_CODE, ACTION_CODE> ACTION_MAP_ITEM;
+
     /*
-     * @description Get next input history
-     * @return the reference of next record
+     * class Crnepl
+     * The most important to implement RPL function
      */
-    std::string & GetNextInput();
+    class Crnepl
+    {
+    public:
+        Crnepl();
+        
+        /* @description Add a record to the history queue
+         * @return void
+         */
+        void AddHistory(std::string &record);
+        /*
+         * @description Get previous input history
+         * @return the reference of previous record
+         */
+        std::string & GetPrevInput();
+        /*
+         * Get current input history
+         * @return the reference of current record
+         */
+        std::string & GetCurInput();
+        /*
+         * @description Get next input history
+         * @return the reference of next record
+         */
+        std::string & GetNextInput();
 
-protected:
-    int RoundPos(int iPos) { return (iPos+HISTORY_SIZE) % HISTORY_SIZE; }
+    protected:
+        int RoundPos(int iPos) { return (iPos+HISTORY_SIZE) % HISTORY_SIZE; }
+    private:
+        /*
+         * @description Initialize sytem action map, cannot be modified
+         * @return void
+         */
+        void InitSysActionMap();
+        /*
+         * @description Initialize user action map, can be customed
+         * @return void
+         */
+        void InitUserActionMap();
 
-public:
-    const int HISTORY_SIZE = 100; //max size of historical input
-private:
-    std::string prompt; // prompt string
-    std::istream *pInput; //input stream
-    std::ostream *pOutput; //output stream
+    public:
+        const int HISTORY_SIZE = 100; //max size of historical input
+    private:
+        std::string prompt; // prompt string
+        std::istream *pInput; //input stream
+        std::ostream *pOutput; //output stream
 
-    std::string aHistoryRecord[HISTORY_SIZE]; //record the history input, this is a loop queue
-    int iHisBeginPos; // begin pos of his queue;
-    int iHisEndPos; // end pos of his queue;
-    int iHisCurPos; // cur pos of his queue;
-};
+        std::map<KEY_CODE,ACTION_CODE> oSysActionMap;
+        std::map<KEY_CODE,ACTION_CODE> oUserActionMap;
 
+        std::string aHistoryRecord[HISTORY_SIZE]; //record the history input, this is a loop queue
+        int iHisBeginPos; // begin pos of his queue;
+        int iHisEndPos; // end pos of his queue;
+        int iHisCurPos; // cur pos of his queue;
+    };
+}
 #endif
--- src/crnepl.cpp
+++ src/crnepl.cpp
@@ -11,6 +11,9 @@ Crnepl::Crnepl()
     iHisBeginPos = 0;
     iHisEndPos = HISTORY_SIZE-1;
     iHisCurPos = 0;
+
+    InitSysActionMap();
+    InitUserActionMap();
 }
 
 void Crnepl::AddHistory(string &record)
@@ -27,7 +30,7 @@ void Crnepl::AddHistory(string &record)
     iHisCurPos = iHisBeginPos;
 }
 
-string & GetPrevInput()
+string & Crnepl::GetPrevInput()
 {
     if (iHisCurPos != iHisEndPos)
     {
@@ -36,7 +39,7 @@ string & GetPrevInput()
     return aHistoryRecord[iHisCurPos];
 }
 
-string & GetNextInput()
+string & Crnepl::GetNextInput()
 {
     if (iHisCurPos != iHisBeginPos)
     {
@@ -45,8 +48,22 @@ string & GetNextInput()
     return aHistoryRecord[iHisCurPos];
 }
 
-string & GetCurInput()
+string & Crnepl::GetCurInput()
 {
     return aHistoryRecord[iHisCurPos];
 }
 
+void Crnepl::InitSysActionMap()
+{
+    oSysActionMap.insert(ACTION_MAP_ITEM(KEY_CTRL_J, ACT_SUBMIT));
+    oSysActionMap.insert(ACTION_MAP_ITEM(KEY_CTRL_H, ACT_BS));
+    oSysActionMap.insert(ACTION_MAP_ITEM(KEY_CTRL_D, ACT_DEL));
+}
+
+void Crnepl::InitUserActionMap()
+{
+    oUserActionMap.insert(ACTION_MAP_ITEM(KEY_CTRL_U, ACT_CLEAR_LINE));
+    oUserActionMap.insert(ACTION_MAP_ITEM(KEY_CTRL_P, ACT_PRE));
+    oUserActionMap.insert(ACTION_MAP_ITEM(KEY_CTRL_N, ACT_NEXT));
+    oUserActionMap.insert(ACTION_MAP_ITEM(KEY_CTRL_I, ACT_COMPLETION));
+}
